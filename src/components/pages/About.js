3112fd374ae063e6ede60e4381a3d98cf9f1a613
Unknown <huynhnguyen27091994@gmail.com>
2019-08-05 10:00:23 +0700

    Transfer project from React Repository

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, {fragment} from 'react'
+
+const About = () => {
+    return (
+        <fragment>
+            <h1>About This App</h1>
+            <p>App to search Github User</p>
+            <p>Version: 1.0.0</p>
+        </fragment>
+    )
+}
+
+export default About
